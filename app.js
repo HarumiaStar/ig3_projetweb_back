342ed6dd7263d536337496b728bd4402cca29b11
root <root@JetbrainsGateway.local>
2023-04-15 11:36:24 +0000

    feat: cors

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/userRoute');
 
 const app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
