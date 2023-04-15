53972e85e17a44bd95fdadc3d55fc85bcac9022a
root <root@JetbrainsGateway.local>
2023-04-15 09:34:24 +0000

    fix(user): update & delete without hash and salt

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,8 +20,11 @@ exports.create = function (req, res, next){
     if (!regexPassword(password)) return res.status(500).send({error: "Le mot de passe doit contenir au moins huit caractères, dont au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial."});
     user.setPassword(password);
     user.save().then(() => {
-        // req.session.user = user; // A supprimer ou remplacer
-        res.json(userData);
+        if (userData) return res.json(userData);
+        throw new Error();
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send({error : "Impossible sauvegarder cet utilisateur."});
     });
 };
 
@@ -33,7 +36,11 @@ exports.create = function (req, res, next){
  */
 exports.readAll = function (req, res, next) {
     User.find({}).select(["-hash", "-salt"]).then((resp) => {
-        res.json(resp);
+        if (resp) return res.json(resp);
+        throw new Error();
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send({error : "Impossible de lire la liste des utilisateurs."});
     });
 };
 
@@ -44,8 +51,12 @@ exports.readAll = function (req, res, next) {
  * @param {*} next 
 */
 exports.read = function (req, res, next) {
-    User.findById(req.params.id).then((resp) => {
-        res.json(resp);
+    User.findById(req.params.id).select(["-is_admin", "-hash", "-salt"]).then((resp) => {
+        if (resp) return res.json(resp);
+        throw new Error();
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send({error : "Impossible de lire les informations de cet utilisateur."});
     });
 };
 
@@ -57,25 +68,32 @@ exports.read = function (req, res, next) {
 */
 exports.update = function (req, res, next){
     const reqBody = req.body;
-    User.findById(req.params.id).select(["-is_admin", "-hash", "-salt"]).then((u) =>{
-        if (reqBody.name) u.name = reqBody.name;
-        if (reqBody.first_name) u.first_name = reqBody.first_name;
-        if (reqBody.birthday) u.birthday = reqBody.birthday;
-        if (reqBody.city) u.city = reqBody.city;
-        if (reqBody.email) u.email = reqBody.email;
+    User.findById(req.params.id).select("-is_admin").then((user) =>{
+        if (reqBody.name) user.name = reqBody.name;
+        if (reqBody.first_name) user.first_name = reqBody.first_name;
+        if (reqBody.birthday) user.birthday = reqBody.birthday;
+        if (reqBody.city) user.city = reqBody.city;
+        if (reqBody.email) user.email = reqBody.email;
 
         if (reqBody.newPassword && reqBody.password){
-            if (u.validPassword(reqBody.password)){
+            if (user.validPassword(reqBody.password)){
                 if (!regexPassword(reqBody.newPassword)) return res.status(500).send({error: "Le mot de passe doit contenir au moins huit caractères, dont au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial."});
-                u.setPassword(reqBody.newPassword);
+                user.setPassword(reqBody.newPassword);
             }
+            else return res.status(500).send({error: "Le mot de passe n'est pas correct."});
         }
-        u.save().then(() => {
-            res.json(u);
+        user.save().then(() => {
+            user.hash = undefined;
+            user.salt = undefined;
+            res.json(user);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).send({error : "Impossible de mettre à jour les données cet utilisateur."});
         });
-        /*const password = req.body.password;
-        if (password) u.setPassword(password);*/
-    })
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send({error : "Impossible de mettre à jour cet utilisateur."});
+    });
 };
 
 function regexPassword(password){
@@ -91,7 +109,11 @@ function regexPassword(password){
  * @param {*} next 
 */
 exports.delete = function (req, res, next){
-    User.findByIdAndDelete(req.params.id).then((resp) => {
-        res.json(resp);
+    User.findByIdAndDelete(req.params.id).select(["-hash", "-salt"]).then((resp) => {
+        if (resp) return res.json(resp);
+        throw new Error();
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send({error : "Impossible de supprimer cet utilisateur."});
     });
 };
